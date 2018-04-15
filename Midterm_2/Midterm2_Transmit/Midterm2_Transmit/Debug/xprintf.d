4a8ce9f89492d98c53e1116600c2db42036e4923
Trace Stewart <tracestewart05@gmail.com>
2018-04-15 11:31:28 -0700

    da4

---
+++ Midterm_2/Midterm2_Transmit/Midterm2_Transmit/Debug/xprintf.d
@@ -0,0 +1,6 @@
+xprintf.d xprintf.o: .././xprintf.c .././xprintf.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h
+
+.././xprintf.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
